baa88fff7c622d4250cc4220bbccefecf0683bd9
omaradil28 <163222841+omaradil28@users.noreply.github.com>
2024-03-12 21:51:50 -0700

    Update run.cpp

--- run.cpp
+++ run.cpp
@@ -7,49 +7,68 @@ Run::Run() {}
 Run::~Run() {}
 
 void Run::runSim(){
+
 	srand(time(0));
 	
 	int totalTime = 0;
 	int userTime = 0;
 
-	int expressNum = 0;
-	int normalNum = 0;
+	int custNum = 0;
 
 	int expressArrival = randomNum(1);
 	int normalArrival = randomNum(2);
 
-	int expressEnter = 0;
-	int normalEnter = 0;
+	int expressEnter = 100;
+	int normalEnter = 100;
 
-	int expressDone = 0;
-	int normalDone = 0;
+	Data expressLeave;
+	Data normalLeave;
 	
 	userTime = userChoice();
 
 	while (userTime != totalTime) {
 		
-		if (expressArrival == expressDone) {
-			expressNum++;
+		if (expressArrival == 0) {
+			custNum++;
 
-			enterLane(expressNum, totalTime, 1, express);
+			enterLane(custNum, totalTime, 1, express);
 			expressArrival = randomNum(1);
+			expressEnter = express.getHead()->getData().serviceGet();
 
-			cout << express << endl;
+			cout << "Customer:" << express << "Has Entered Express" << endl;
 			system("pause");
 		}
-		if (normalArrival == normalDone) {
-			normalNum++;
+		if (normalArrival == 0) {
+			custNum++;
 
-			enterLane(normalNum, totalTime, 2, normal);
+			enterLane(custNum, totalTime, 2, normal);
 			normalArrival = randomNum(2);
+			normalEnter = normal.getHead()->getData().serviceGet();
 		
-			cout << normal << endl;
+			cout << "Customer:" << normal << "Has Entered Normal" << endl;
 			system("pause");
+		}
+
+		if (expressEnter == 0) {
+			if (!express.isEmpty())
+			{
+				expressLeave = express.dequeue();
+				cout << expressLeave << "dequeued\n" << endl;
+			}
+		}
+		if (normalEnter == 0) {
+			if (!normal.isEmpty())
+			{
+				normalLeave = normal.dequeue();
+				cout << normalLeave << "dequeued\n" << endl;
+			}
 		}
 		
 		totalTime++;
 		expressArrival--;
 		normalArrival--;
+		expressEnter--;
+		normalEnter--;
 	}
 }
 
@@ -76,7 +95,7 @@ int Run::randomNum(int num) {
 		rando = (rand() % 5) + 1;
 	}
 	else if (num == 2) {
-		rando = (rand() % 8) + 3;
+		rando = (rand() % 6) + 3;
 	}
 	
 	return rando;
@@ -85,7 +104,7 @@ int Run::randomNum(int num) {
 void Run::enterLane(int num, int time, int type, Queue& info) {
 	int service = 0;
 
-	
+	//Skips over these below lines for some reason a
 	if (type == 1) {
 		service = randomNum(type);
 		info.enqueue(Data(num, service, time));
@@ -100,27 +119,14 @@ void Run::enterLane(int num, int time, int type, Queue& info) {
 void Run::runTest() {
 	Data data;
 
-	express.enqueue(Data(1, 2, 3));
-	express.enqueue(Data(4, 5, 6));
+	express.enqueue(Data(1, 2, 3)); // Front of queue
+	express.enqueue(Data(4, 5, 6)); // Back of queue
+
+	express.printQueue(express); // Should print 1,2,3
 
 	if (!express.isEmpty()) {
-		data = express.dequeue();
+		data = express.dequeue(); // Should dequeue 1,2,3 since its the front. Now 4,5,6 should be front of queue.
 	}
 
-	cout << data << endl;
-
-	express.enqueue(Data(7, 8, 9));
-
-	if (!express.isEmpty()) {
-		data = express.dequeue();
-	}
-
-	cout << data << endl;
-
-	if (!express.isEmpty()) {  // Perform another dequeue operation
-		data = express.dequeue();
-	}
-
-	cout << data << endl;  // Should print Data(4, 5, 6), but it does not
-
+	express.printQueue(express); // Should print front of queue AKA 4,5,6, but it doesnt.
 }
